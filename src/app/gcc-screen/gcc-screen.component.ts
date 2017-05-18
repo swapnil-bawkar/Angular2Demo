eb58e99b8c4aac7a4f1874e1be3f9a3444d4fad0
Swapnil Bawkar <Swapnil.Bawkar@synechron.com>
2017-05-18 16:26:39 +0530

    Dynamic tab filter example

--- src/app/gcc-screen/gcc-screen.component.ts
+++ src/app/gcc-screen/gcc-screen.component.ts
@@ -3,17 +3,40 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-gcc-screen',
   template: `
-    <p>
-      Tab2 Works!
-    </p>
+    <p-dataTable [value]="cars">
+        <p-column *ngFor="let col of cols" [field]="col.field" [header]="col.header">
+          <ng-template let-col let-car="rowData" let-ri="rowIndex" pTemplate="body">
+              <span *ngIf="col.field==='year'">ss{{car[col.field]}}</span>
+              <span *ngIf="col.field==='vin'">bin{{car[col.field]}}</span>
+              <span *ngIf="col.field.indexOf('vin', 'year') === -1">normal{{car[col.field]}}</span>
+          </ng-template>          
+        </p-column>
+    </p-dataTable>
   `,
   styles: [``]
 })
 export class GccScreenComponent implements OnInit {
 
+  cars = [{
+    vin: 'abc',
+    year: '2017/05/18',
+    brand: 'PQR',
+    color: 'red'
+  }];
+    
+  cols: any[];
+
   constructor() { }
 
   ngOnInit() {
+      
+      
+      this.cols = [
+          {field: 'vin', header: 'Vin'},
+          {field: 'year', header: 'Year'},
+          {field: 'brand', header: 'Brand'},
+          {field: 'color', header: 'Color'}
+      ];
   }
 
 }
